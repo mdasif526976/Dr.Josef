1b519a157a2ab660b2f56390b8a0da7c93a786a3
mdasif526976 <mdasif526976@gmail.com>
2022-11-08 23:19:37 +0600

    responsite footer add

--- src/shered/Footer.js
+++ src/shered/Footer.js
@@ -1,9 +1,133 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '.././image/Screenshot 2022-11-08 at 23-06-21 Free Vector Turquoise hospital logo template.png'
 
 const Footer = () => {
     return (
         <div>
-            footer
+                <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="sm:col-span-2">
+          <Link
+            
+            title="Company"
+            className="inline-flex items-center"
+          >
+            <img className='w-14' src={logo} alt=''></img>
+            <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+              Company
+            </span>
+          </Link>
+         
+        </div>
+        <div className="space-y-2 text-sm">
+          <p className="text-base font-bold tracking-wide text-gray-900">
+            Contacts
+          </p>
+          <div className="flex">
+            <p className="mr-1 text-gray-800">Phone:</p>
+            <Link
+              to="tel:850-123-5021"
+              aria-label="Our phone"
+              title="Our phone"
+              className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              850-123-5021
+            </Link>
+          </div>
+          <div className="flex">
+            <p className="mr-1 text-gray-800">Email:</p>
+            <Link
+              to="mailto:drjosef@gmail.com"
+              aria-label="Our email"
+              title="Our email"
+              className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              drjosef@gmail.com
+            </Link>
+          </div>
+          <div className="flex">
+            <p className="mr-1 text-gray-800">Address:</p>
+            <Link
+              to="https://www.google.com/maps"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Our address"
+              title="Our address"
+              className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              312 Lovely Street, NY
+            </Link>
+          </div>
+        </div>
+        <div>
+          <span className="text-base font-bold tracking-wide text-gray-900">
+            Social
+          </span>
+          <div className="flex items-center mt-1 space-x-3">
+            <Link
+              to="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <img src='' alt=''></img>
+            </Link>
+            <Link
+              to="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
+                <circle cx="15" cy="15" r="4" />
+                <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
+              </svg>
+            </Link>
+            <Link
+              to="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+              </svg>
+            </Link>
+          </div>
+          <p className="mt-4 text-sm text-gray-500">
+           You Contact with me By Social media.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
+        <p className="text-sm text-gray-600">
+          © Copyright 2022 Lorem Inc. All rights reserved.
+        </p>
+        <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+          <li>
+            <Link
+              to="/"
+              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              F.A.Q
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className="text-sm text-gray-600 transition-colors duration-300
+               hover:text-deep-purple-accent-400"
+            >
+              Privacy Policy
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className="text-sm text-gray-600 transition-colors duration-300 
+              hover:text-deep-purple-accent-400"
+            >
+              Terms &amp; Conditions
+            </Link>
+          </li>
+        </ul>
+      </div>
+      </div>
         </div>
     );
 };
